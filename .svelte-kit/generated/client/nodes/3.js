c57f9227c81162be177593b5ab18a05b51b5f393
Mariana Villamizar <marianaviro93@gmail.com>
2024-04-03 15:44:55 -0400

    Reverted and stopped tracking svelte-kit folder

--- .svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/color_shihpo/+page.js";
-export { universal };
-export { default as component } from "../../../../src/routes/color_shihpo/+page.svelte";
