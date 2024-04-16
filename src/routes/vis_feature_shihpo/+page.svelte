844f16b9d3f93e43b11e5c94260fc8294641b4fb
Shih-Po Lee <robert80203@users.noreply.github.com>
2024-04-16 00:46:34 -0400

    robert update

--- src/routes/vis_feature_shihpo/+page.svelte
+++ src/routes/vis_feature_shihpo/+page.svelte
@@ -2,7 +2,7 @@
 	import * as d3 from 'd3';
 	// import Scatterplot from './Scatterplot.svelte';
 	import './style.css';
-	// import Details from './Details.svelte';
+	import Details from './Details.svelte';
 	// import Details_selected from './Details_selected.svelte';
 	// import Scatterplot_Selected from './Scatterplot_selected.svelte';
 	import ColorHue from './ColorHue.svelte';
@@ -18,7 +18,17 @@
 	// let bFeature = 'lc_tsne3_f3';
 	let colorFeature = 'color';
 	let emotionFeature = 'emotion';
-	let feature_maps = 'empty';
+	// let lc_colorhue = none;
+	// let lb_colorhue = none;
+	// let la_colorhue = none;
+	let lc_colorhue = new Array(8);
+	let lb_colorhue = new Array(8);
+	let la_colorhue = new Array(8);
+	for (let i = 0; i < 8; i ++){
+		lc_colorhue[i] = new Array(4);
+		lb_colorhue[i] = new Array(4);
+		la_colorhue[i] = new Array(4);
+	}
 	let id = 1;
 	let idx = 0;
 
@@ -61,9 +71,94 @@
 	async function onclick(){
 		selectedImage = data.dataset[idx];
 		// console.log(selectedImage.lc_feat_path);
-		feature_maps = await n.load("http://localhost:5173/"+selectedImage.lc_feat_path);
-		var feat_array = feature_maps['data'];
-		console.log(feat_array);
+		var lc_feature_maps = await n.load("http://localhost:5173/"+selectedImage.lc_feat_path);
+		var lb_feature_maps = await n.load("http://localhost:5173/"+selectedImage.lb_feat_path);
+		var la_feature_maps = await n.load("http://localhost:5173/"+selectedImage.la_feat_path);
+		var lc_feat_array = lc_feature_maps['data'];
+		var lb_feat_array = lb_feature_maps['data'];
+		var la_feat_array = la_feature_maps['data'];
+		console.log(lc_feat_array.length);
+		console.log(lb_feat_array.length);
+		console.log(la_feat_array.length);
+		
+		for (let i = 0; i < 8; i ++){
+			for (let j = 0; j < 4; j++){
+				lc_colorhue[i][j] = 0;
+				lb_colorhue[i][j] = 0;
+				la_colorhue[i][j] = 0;
+			}
+		}
+		
+		var lc_max = d3.max(lc_feat_array, d => d);
+		var lc_min = d3.min(lc_feat_array, d => d);
+		var lb_max = d3.max(lb_feat_array, d => d);
+		var lb_min = d3.min(lb_feat_array, d => d);
+		var la_max = d3.max(la_feat_array, d => d);
+		var la_min = d3.min(la_feat_array, d => d);
+
+		// lc_feat_array = (lc_feat_array - lc_max) / (lc_max - lc_min);
+		lc_feat_array = d3.map(lc_feat_array, d => (d - lc_min) / (lc_max - lc_min));
+		lb_feat_array = d3.map(lb_feat_array, d => (d - lb_min) / (lb_max - lb_min));
+		la_feat_array = d3.map(la_feat_array, d => (d - la_min) / (la_max - la_min));
+		
+		console.log('frst', lc_colorhue);
+		
+		for (let i = 0; i < 8; i++){
+			for (let j = 0; j < lc_feat_array.length / 32; j++){
+				lc_colorhue[i][0] += lc_feat_array[j + i * (lc_feat_array.length / 8)];
+			}
+			for (let j = 0; j < lb_feat_array.length / 32; j++){
+				lb_colorhue[i][0] += lb_feat_array[j + i * (lb_feat_array.length / 8)];
+			}
+			for (let j = 0; j < la_feat_array.length / 32; j++){
+				la_colorhue[i][0] += la_feat_array[j + i * (la_feat_array.length / 8)];
+			}
+			
+			
+			for (let j = lc_feat_array.length / 32; j < lc_feat_array.length / 24; j++){
+				lc_colorhue[i][1] += lc_feat_array[j + i * (lc_feat_array.length / 8)];
+			}
+			for (let j = lb_feat_array.length / 32; j < lb_feat_array.length / 24; j++){
+				lb_colorhue[i][1] += lb_feat_array[j + i * (lb_feat_array.length / 8)];
+			}
+			for (let j = la_feat_array.length / 32; j < la_feat_array.length / 24; j++){
+				la_colorhue[i][1] += la_feat_array[j + i * (la_feat_array.length / 8)];
+			}
+			
+			// something wrong, don't know
+			for (let j = lc_feat_array.length / 24; j < lc_feat_array.length / 16; j++){
+				lc_colorhue[i][2] += lc_feat_array[j + i * (lc_feat_array.length / 8)];
+			}
+			for (let j = lb_feat_array.length / 24; j < lb_feat_array.length / 16; j++){
+				lb_colorhue[i][2] += lb_feat_array[j + i * (lb_feat_array.length / 8)];
+			}
+			for (let j = la_feat_array.length / 24; j < la_feat_array.length / 16; j++){
+				la_colorhue[i][2] += la_feat_array[j + i * (la_feat_array.length / 8)];
+			}
+		
+
+			for (let j = lc_feat_array.length / 16; j < lc_feat_array.length / 8; j++){
+				lc_colorhue[i][3] += lc_feat_array[j + i * (lc_feat_array.length / 8)];
+			}
+			for (let j = lb_feat_array.length / 16; j < lb_feat_array.length / 8; j++){
+				lb_colorhue[i][3] += lb_feat_array[j + i * (lb_feat_array.length / 8)];
+			}
+			for (let j = la_feat_array.length / 16; j < la_feat_array.length / 8; j++){
+				la_colorhue[i][3] += la_feat_array[j + i * (la_feat_array.length / 8)];
+			}
+		}
+		// console.log(lc_colorhue);
+		for (let i = 0; i < 8; i ++){
+			for (let j = 0; j < 4; j++){
+				lc_colorhue[i][j] /= (lc_feat_array.length / 32);
+				lb_colorhue[i][j] /= (lb_feat_array.length / 32);
+				la_colorhue[i][j] /= (la_feat_array.length / 32);
+			}
+		}
+
+		// console.log(lc_colorhue);
+		console.log(lb_colorhue);
+		
 		// console.log(selectedImage);
 		// console.log(feature_maps['data']);
 		idx += 1;
@@ -113,16 +208,18 @@
 
 <div class="container">
 	<div class="header">
-		<FeatureControls dataset={data.dataset} bind:xFeature bind:yFeature bind:colorFeature />
-		<ColorLegend {color} />
 		<button on:click={() => onclick()}>
 			Select
 		</button>
 	</div>
 	<div class="main">
 		<ColorHue
+			bind:lc_colorhue={lc_colorhue}
+			bind:lb_colorhue={lb_colorhue}
+			bind:la_colorhue={la_colorhue}
+		/>
+		<Details
 			{selectedImage}
-			{feature_maps}
 		/>
 	</div>
 	
--- src/routes/vis_feature_shihpo/ColorHue.svelte
+++ src/routes/vis_feature_shihpo/ColorHue.svelte
@@ -1,23 +1,59 @@
 <script>
-	export let selectedImage;
-	export let feature_maps;
+	export let lc_colorhue;
+	export let lb_colorhue;
+	export let la_colorhue;
 	// draw();
-	
+	let deg = 45;
+
+	// 	Convert deg number to string for CSS
+	$: lc_c1 = `rgba(${Math.floor(lc_colorhue[0][0]*255)}, 
+				     ${Math.floor(lc_colorhue[0][1]*255)},
+					 ${Math.floor(lc_colorhue[0][3]*255)})`
+	$: lc_c2 = `rgba(${Math.floor(lc_colorhue[1][0]*255)}, 
+					 ${Math.floor(lc_colorhue[1][1]*255)}, 
+					 ${Math.floor(lc_colorhue[1][3]*255)})`
+	$: degString = `${deg}deg`
+
+	//console.log(lc_colorhue[0][2]);
+	//console.log(Math.floor(lc_colorhue[0][0]*255));
 </script>
 
 
-<div class="shorter">
-	{#if selectedImage}
-		<img src={selectedImage.file_path} width='250px' alt="" />
-		<h3>{selectedImage.emotion}</h3>
-	{/if}
-</div>
+<!-- <div class="shorter">
+	
+</div> -->
+
+<div
+	style="
+		--gradient-1:{lc_c1};
+		--gradient-2:{lc_c2};
+	"
+	
+></div>
+<div
+	style="
+		--gradient-1:rgba(140, 44, 235);
+		--gradient-2:rgba(144, 52, 257);
+	"
+	
+></div>
+<h1>
+	{lc_c1}
+	{lc_c2}
+</h1>
+
+<!-- <div
+	style="
+		--gradient-1:rgba(255,0,0,0.2);
+		--gradient-2:rgba(123,123,0,0.2);
+	"
+></div> -->
 <!-- <div class="longer">longer hue</div> -->
 
 
 
 <style>
-	div {
+	/* div {
 		height: 50vh;
 		width: 50%;
 		color: white;
@@ -25,8 +61,16 @@
 	}
 
 	.shorter {
-		/* background: linear-gradient(90deg in hsl shorter hue, red, blue); */
-		background: linear-gradient(red 0%, orange 25%, yellow 50%, green 75%, blue 100%);
+		background: linear-gradient(rgba(255,0,0,0.2) 0%, rgba(120,120,0,0.4 ) 25%, yellow 50%, green 75%, blue 100%);
+	} */
+	div {
+		margin: 10px;
+		width: 200px;
+		height: 200px;
+		border: 2px solid black;
+		background: linear-gradient(var(--gradient-1), 
+									var(--gradient-2)
+									)
 	}
 
 	/* .longer {
